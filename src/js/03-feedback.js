f5d78051a787336c2114ea9cc192d28f67001a59
pavel11988 <kossoua88@gmail.com>
2021-12-19 20:05:16 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,22 +8,40 @@ const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input');
 
 onMessageOutput();
+onSaved();
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onTextInput, 500));
 form.addEventListener('input', throttle(onConsoleOutput, 500));
 
+function onSaved () {
+    data.email = input.value;
+    data.message = textarea.value;
+}
+
+function onConsoleOutput(event){
+    data[event.target.name] = event.target.value;
+}
+
 function onFormSubmit (event) {
     event.preventDefault();
-    event.currentTarget.reset();
-    localStorage.removeItem(MESSAGE_KEY);
-    if(Object.keys(data).length === 0 && Object.values(data).length === 0) return;
-    console.log(data);
 
+    if(!(event.currentTarget.elements.email.value === '' || event.currentTarget.elements.message.value === ''))
+    {
+        event.currentTarget.reset();
+        localStorage.removeItem(MESSAGE_KEY);
+        if(Object.keys(data).length === 0 && Object.values(data).length === 0) return;
+        console.log(data);
+        
+    } else{
+    alert('Все поля должны быть заполнены!');
+    return;}
 }
 
 
 function onTextInput (event) {
+
+
     if(Object.keys(data).length === 0) return;
     const dataStr = JSON.stringify(data);
     localStorage.setItem(MESSAGE_KEY, dataStr);
@@ -38,6 +56,3 @@ function onMessageOutput () {
 }
 
 
-function onConsoleOutput(event){
-    data[event.target.name] = event.target.value;
-}
